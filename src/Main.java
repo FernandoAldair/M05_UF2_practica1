455ec39df727f96c5cfd982afef4f6c9e242a6c3
fernando <fcano@elpuig.xeill.net>
2021-01-31 19:32:47 +0100

    direc2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola mundo_0");
-        System.out.println("Hola mundo_1");
+        System.out.println("Hola mundo_2");
 
     }
 }
