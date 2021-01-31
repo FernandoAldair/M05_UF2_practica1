2d85ecda9dec7adc66c3e5f14f27e048807a3f53
fernando <fcano@elpuig.xeill.net>
2021-02-01 00:47:36 +0100

    direc1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,55 @@
 public class Main {
     public static void main(String[] args) {
+//        /**
+//         * @param persona: Es nuestro sujeto que nos confirmaran si esan caminando o corriendo
+//         * Print.
+//         * 1-Mira el numero generado aleatoriamente
+//         * 2-Comprueba si es menor(caminando) o mayor(corriendo) que 5
+//         * 3-Nos dice si la persona esta caminando o corriendo
+//         */
+        Persona persona1 = new Persona("juan",23) ;
+        Persona persona2 = new Persona("Carlos", 28);
 
-        System.out.println("Hola mundo_0");
-        System.out.println("Hola mundo_1");
 
+
+        if (persona1.getNumeroAleatorio() > 5){
+            System.out.println("juan esta corriendo");
+        }else {
+            System.out.println("Juan esta caminando");
+        }
+
+        if (persona2.getNumeroAleatorio() > 5){
+            System.out.println("Carlos esta corriendo");
+        }else {
+            System.out.println("Carlos esta caminando");
+        }
+
+    }
+
+    public static class Persona{
+
+        String nombre;
+        int edad;
+        int numeroAleatorio = (int) (Math.random()*10+1);
+
+        /**
+         *
+         * Getter.
+         * @return Numero generado aleatoriamente
+         *
+         */
+        public int getNumeroAleatorio() {
+            return numeroAleatorio;
+        }
+        /**
+         * Constructor.
+         * @param nombre: Nombre de la Persona.
+         * @param edad: Edad de la Persona
+         *
+         */
+        public Persona(String nombre, int edad) {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
     }
 }
